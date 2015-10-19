ba9bfc0a668f902983fe131c15de75b35cf10942
zhuotao25 <zhuotaohuang@umass.edu>
2015-10-19 12:02:56 -0400

    set up the project

---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var app = express();
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function(req, res){
+	res.type('text/plain');
+	res.send('CS326 Turkey');
+});
+
+app.get('/about', function(req, res){
+	res.type('text/plain');
+	res.send('About Turkey');
+});
+
+app.get('/team', function(req, res){
+	res.type('text/plain');
+	res.send('Team');
+});
+
+//custom 404 page
+app.use(function(req, res){
+	res.type('text/plain');
+	res.status(404);
+	res.send('404 - Not Found');
+});
+//custom 500 page
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.type('text/plain');
+	res.status(500);
+	res.send('500 - Server Error');
+});
+app.listen(app.get('port'), function(){
+	console.log( 'Express started on http://localhost:' +
+			app.get('port') + '; press Ctrl-C to terminate.' );
+});
