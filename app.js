e3e6b99d8bed11b233333a4d2a5fa170a7b6a83d
zhuotao25 <zhuotaohuang@umass.edu>
2015-10-19 12:58:12 -0400

    set up views, handlebars, create /public

--- app.js
+++ app.js
@@ -2,34 +2,39 @@ var express = require('express');
 var app = express();
 app.set('port', process.env.PORT || 3000);
 
+//set up handlebars view engine
+var handlebars = require('express3-handlebars').create({ defaultLayout:'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res){
 	res.type('text/plain');
 	res.send('CS326 Turkey');
 });
 
 app.get('/about', function(req, res){
-	res.type('text/plain');
-	res.send('About Turkey');
+	res.render('about');
 });
 
 app.get('/team', function(req, res){
-	res.type('text/plain');
-	res.send('Team');
+	res.render('team');
 });
 
 //custom 404 page
 app.use(function(req, res){
-	res.type('text/plain');
 	res.status(404);
-	res.send('404 - Not Found');
+	res.render('404');
 });
+
 //custom 500 page
 app.use(function(err, req, res, next){
 	console.error(err.stack);
-	res.type('text/plain');
 	res.status(500);
-	res.send('500 - Server Error');
+	res.render('500');
 });
+
 app.listen(app.get('port'), function(){
 	console.log( 'Express started on http://localhost:' +
 			app.get('port') + '; press Ctrl-C to terminate.' );
